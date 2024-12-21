6c96c6bde71324953d4e0f0c1ccc912655d8e0e4
rahul-ankamreddi <rahul.ankamreddi@gmail.com>
2024-12-21 16:55:10 +0530

    Added App and MainWindow

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-if __name__ == 'main':
-    pass
+# built-ins
+import sys
+
+# installed
+from PyQt5.QtWidgets import QApplication
+
+# local
+from MainWindow import MainWindow
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec_()
