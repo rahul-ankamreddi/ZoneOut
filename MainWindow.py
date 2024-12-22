d8043b0d3067f6bfe4698c9a56012dade9affcca
rahul-ankamreddi <rahul.ankamreddi@gmail.com>
2024-12-22 08:29:09 +0530

    added display_time and connected button to it

--- MainWindow.py
+++ MainWindow.py
@@ -1,3 +1,6 @@
+# built-in
+import time
+
 # installed
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
@@ -13,11 +16,12 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout()
 
         capture_button = QPushButton("Capture")
-
+        capture_button.pressed.connect(self.display_time)
         layout.addWidget(capture_button)
 
         widget = QWidget()
-
         widget.setLayout(layout)
-
         self.setCentralWidget(widget)
+
+    def display_time(self):
+        print(time.ctime())
